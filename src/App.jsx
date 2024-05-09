47af5b68371635823a991ff7473692beb92c934e
Hwayeong <joid1004@naver.com>
2024-05-09 16:04:53 +0900

    배열의 메소드 push,pop,map

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
 import React from "react";
 
 function App(props) {
-  const a = {};
-  console.log("a", typeof a);
-  const b = []; // 배열이라는 객체
-  console.log("b", typeof b);
+  // 배열 메소드
+  const a = [];
 
-  // 배열 : 여러 값을 순서(index)대로 가지고 있는 객체
+  // push : 새 값을 배열 맨 뒤에 추가
+  a.push("흥민");
+  a.push("강인");
+  a.push(3.11);
+  console.log(a);
 
-  const c = ["son", "lee", "kim", "hwang"];
-  console.log("c", c);
-  console.log("c의 0", c[0]);
-  console.log("c의 1", c[1]);
-  console.log("c의 2", c[2]);
-  // 없는 인덱스에도 접근 가능
-  console.log("c의 5", c[5]); // undefined
-  return (
-    <div>
-      <ul>
-        <li>{c[0]}</li>
-        <li>{c[1]}</li>
-        <li>{c[2]}</li>
-        <li>{c[3]}</li>
-      </ul>
-    </div>
-  );
+  // pop : 맨 뒤의 값을 제거 후 리턴
+  let val1 = a.pop();
+  let val2 = a.pop();
+  console.log(val1);
+  console.log(val2);
+  console.log(a);
+
+  // map : 각 원소를 다른 값으로 매핑(map) 후 새 배열 리턴
+  const b = [5, 4, 3, 2, 1];
+  console.log(b);
+  // 함수를 정의하고 넣음
+  function action(param) {
+    return param * 2;
+  }
+  const c = b.map(action);
+  console.log(b);
+  console.log(c);
+
+  //
+  const d = b.map(function (param) {
+    return param * 3;
+  });
+  console.log(d);
+
+  let aa = b.map(action);
+  console.log(aa);
+
+  let bb = b.map((a) => a * 7);
+  console.log(bb);
+
+  return <div></div>;
 }
 
 export default App;
