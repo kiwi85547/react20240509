f2760c0e637ba405a6a424510e183e0a75bf4c0a
Hwayeong <joid1004@naver.com>
2024-05-09 15:29:08 +0900

    자바스크립트 객체(Object)도 참조값을 저장한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
 function App(props) {
-  return;
-  <div></div>;
+  // object
+  const a = { name: "John" };
+  const b = a;
+
+  console.log(a === b); // 항상 true
+
+  console.log("a.name", a.name); //John
+  console.log("b.name", b.name); //John
+
+  // javascript도 참조값을 가지고 있음
+  b.name = "Jane";
+  console.log(a === b); // 항상 true. 객체가 바뀐것이 아님..
+
+  console.log("a.name", a.name); //Jane
+  console.log("b.name", b.name); //Jane
+
+  return <div></div>;
 }
 
 export default App;
