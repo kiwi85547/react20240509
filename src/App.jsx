34f1fe8f69304c6de09b54a8826ab39d332dfcf8
Hwayeong <joid1004@naver.com>
2024-05-09 15:16:24 +0900

    javascript object와 property 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 function App(props) {
-  const a = {
-    name: "흥민",
-    team: "토트넘",
-    salary: 500,
+  const obj = {
+    age: 30,
+    name: "John Doe",
+    email: "johhn@a.com",
+    password: "1234",
   };
 
   return (
     <div>
-      {a.name}은 {a.team} 소속이고 연봉은 {a.salary}억원 이다.
+      {/*  obj의 각 프로퍼티를 jsx 내에서 출력하는 코드 작성*/}
+      나이 : {obj.age} <br /> 이름 : {obj.name} <br />
+      이메일 : {obj.email} <br /> 패스워드 : {obj.password}
     </div>
   );
 }
