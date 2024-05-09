f062bd763ea8d59abf88a280afb8ff2b947e3273
Hwayeong <joid1004@naver.com>
2024-05-09 12:15:24 +0900

    jsx 내에서 {}로 자바스크립트 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+// App() 은 컴포넌트
 function App() {
-  return <></>;
+  // {} 사용해서 javascript 코드 작성 가능
+  const v1 = 5;
+  const v2 = 7;
+
+  // someAction()은 함수
+  function someAction() {
+    return "흥민";
+  }
+  function someAction2(p) {
+    return p * p;
+  }
+  return (
+    <>
+      <p>
+        {2}+{3}={2 + 3}
+      </p>
+      <p>
+        {v1}+{v2}={v1 + v2}
+      </p>
+      <p>
+        {v1}*{v2}={v1 * v2}
+      </p>
+      <p>{someAction()}</p>
+      <p>{someAction2(5)}</p>
+      <p>
+        <i>{v1 < 10 ? "작다" : "크다"}</i>
+      </p>
+    </>
+  );
 }
 export default App;
