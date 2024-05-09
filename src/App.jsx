21e8bc80fb41af2b005609a6173acecbe7c23d36
Hwayeong <joid1004@naver.com>
2024-05-09 12:02:59 +0900

    jsx에서 자바스크립트 값 사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
-function MyComponent() {
-  // 최상위 컴포넌트 사용하지 않을 때 -> fragment <></>사용
+function App() {
+  // 변수
+  // const, let 중에 사용
+  const myTag = <h1>hello</h1>;
+  const val1 = "흥민";
+  const val2 = 3.14;
+  const val3 = 456;
+  const val5 = true;
+  const val6 = null;
+  const val7 = undefined;
+  const val8 = [2, 3];
+  const val9 = { age: 3, name: "강인" };
 
+  // {} : jsx 내에서 javascript 코드 사용
   return (
     <>
-      <p>Lorem ipsum.</p>
-      <p>Nisi, perferendis.</p>
+      <p>
+        {val1} 선수는 연봉이 {val3}억원이다.
+      </p>
+      <div>{val1}</div>
+      <div>{val2}</div>
+      <div>{val3}</div>
+      <div>{myTag}</div>
+      {/*boolean 은 출력 안됨*/}
+      <div>{val5}</div>
+      {/*null은 출력 안됨*/}
+      <div>{val6}</div>
+      {/*undefined 출력 안됨*/}
+      <div>{val7}</div>
+
+      {/*23으로 출력됨*/}
+      <div>{val8}</div>
+
+      {/*객체는 오류*/}
+      {/*<div>{val9}</div>*/}
+      {/*프로퍼티에 접근해서 사용*/}
+      <div>{val9.name}</div>
+      <div>{val9.age}</div>
     </>
-  );
-}
-
-function MyComp() {
-  // 최상위 컴포넌트는 1개임
-  // 여러 줄 써야한다면, div로 감싸서 쓰기
-  return (
-    <div>
-      <p>Lorem ipsum.</p>
-      <p>Cumque, iste!</p>
-    </div>
-  );
-}
-
-function MyTag() {
-  // jsx 코드가 여러 줄 일 때 () 사용
-  // prettier가 도움을 준다
-  return (
-    <div>
-      hello tag
-      <p>lorem</p>
-    </div>
-  );
-}
-function App() {
-  return (
-    <div>
-      <MyTag />
-      <MyComp />
-      <MyComponent />
-    </div>
   );
 }
 
