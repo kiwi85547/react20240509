112feacf0fd8c51f5f009bca88d8f016434cd3d8
Hwayeong <joid1004@naver.com>
2024-05-09 14:48:01 +0900

    javascript object

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-function App() {
-  // javascript review
+import React from "react";
 
-  // javascript data type
-  let a; // 값을 안 주면 undefined
+function App(props) {
+  // object : {}
+  // {} 안에 property명 : property값 쌍들이 작성됨
+  let a = {};
+  let b = { name: "흥민" };
+  let c = { address: "신촌", city: "서울", country: "한국" };
+
   console.log("a", a);
-  let b = "문자열 타입"; //string
   console.log("b", b);
-
-  // 정수형,실수형으로 나누어 지지 않음
-  let c = 3; //number 타입
-  console.log("c", typeof c);
-  let d = 3.14; // number 타입.
-  console.log("d", typeof d);
-  let e = true; // boolean 타입
-  console.log("e", typeof e);
-
-  let f = {}; // object 타입
-  console.log("f", typeof f);
-
-  let g = function () {}; // function 타입
-  console.log("g", typeof g);
-  // 자바스크립트에서는 값이 들어갈 때 타입이 확정 됨
-  g = {};
-  console.log("g", typeof g);
+  console.log("c", c);
+  console.log("c", c.city);
 
   return <div></div>;
 }
+
 export default App;
