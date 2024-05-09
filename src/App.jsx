1b56743b1255aaccf404f5aa912d5875bc11d5ff
Hwayeong <joid1004@naver.com>
2024-05-09 15:38:58 +0900

    자바스크립트 배열

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
 function App(props) {
-  // object
-  const a = { name: "John" };
-  const b = a;
+  const a = {};
+  console.log("a", typeof a);
+  const b = []; // 배열이라는 객체
+  console.log("b", typeof b);
 
-  console.log(a === b); // 항상 true
+  // 배열 : 여러 값을 순서(index)대로 가지고 있는 객체
 
-  console.log("a.name", a.name); //John
-  console.log("b.name", b.name); //John
-
-  // javascript도 참조값을 가지고 있음
-  b.name = "Jane";
-  console.log(a === b); // 항상 true. 객체가 바뀐것이 아님..
-
-  console.log("a.name", a.name); //Jane
-  console.log("b.name", b.name); //Jane
-
-  return <div></div>;
+  const c = ["son", "lee", "kim", "hwang"];
+  console.log("c", c);
+  console.log("c의 0", c[0]);
+  console.log("c의 1", c[1]);
+  console.log("c의 2", c[2]);
+  // 없는 인덱스에도 접근 가능
+  console.log("c의 5", c[5]); // undefined
+  return (
+    <div>
+      <ul>
+        <li>{c[0]}</li>
+        <li>{c[1]}</li>
+        <li>{c[2]}</li>
+        <li>{c[3]}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
