9076cb3ef6bd3d565259bf7175b8e7ad69bd5623
Hwayeong <joid1004@naver.com>
2024-05-10 16:34:39 +0900

    style props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-function MyComp({headColor,bodyColor}){
-    return(
-        <div>
-            <h1 style={{color:headColor}}>lorem</h1>
-            <p style={{color:bodyColor}}>Lorem ipsum dolor.</p>
-    </div>)
+function MyButton({name,sx}){
+    return <button style={sx}>{name}</button>;
 }
 
 function App(props) {
   return (
     <div>
-        <MyComp headColor={"skyblue"} bodyColor={"gold"}/>
-        <MyComp headColor={"#ccc"}/>
-        <MyComp headColor={"aqua"} bodyColor={"red"}/>
+        <MyButton name={"red button"} sx={{
+        backgroundColor: "red",
+        color: "green",
+        padding:"10px"}
+        }/>
+
     </div>
   );
 }
