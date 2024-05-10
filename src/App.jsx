7fc745f552ed98c2d772a6c83de79d228bca1bac
Hwayeong <joid1004@naver.com>
2024-05-10 15:57:05 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
 import React from "react";
 
 function App(props) {
-  const s1 = "some string";
-  const s2 = "other string";
-  const s3 = "I'm string";
-  const s4 = '"리액트"공부 중';
-
-  // backtick
-  // template literal
-  const s5 = `another string`;
-
-  const name = "son";
-  const age = 30;
-
-  const s6 = name + " is " + age * 2 + " years.";
-  const s7 = `${name} is ${age * 2} years.`;
-
   return (
-    <div>
-      <p>{s6}</p>
-      <p>{s7}</p>
-    </div>
+    <div></div>
   );
 }
 
