2251b9094eeb49e1b0691a561b63ce1fb86934da
Hwayeong <joid1004@naver.com>
2024-05-10 16:10:56 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,7 @@ import React from "react";
 
 function App(props) {
   return (
-    <div>
-      <h1 style={{
-          color:"red",
-          padding:"10px",
-          border:"2px dotted black",
-          textAlign:"center"
-      }}
-      >
-          react style</h1>
-    </div>
+    <div></div>
   );
 }
 
