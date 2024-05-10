aa7ca7fb9cd9e0df53295c8d4f10bbc6bd4a759a
Hwayeong <joid1004@naver.com>
2024-05-10 18:09:49 +0900

    onClick property와 함수 사용

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 import {Button, ChakraProvider} from "@chakra-ui/react";
 
 function App(props) {
-    return (<></>
+    // 이름 있는 함수
+    function action1(){
+        console.log("action1 함수");
+    }
+    // action1(); // 함수 실행.. 없어도 상관없는데??
+
+    // 이름 없는 함수
+    const action2=function(){
+        console.log("anonymous function. 변수 이름은 action2");
+    };
+
+    return (
+        <div>
+            {/*함수 이름이 action1*/}
+            <button onClick={action1}>액션1클릭</button><br/>
+            {/*익명함수 변수 이름이 action2*/}
+            <button onClick={action2}>액션2클릭</button><br/>
+            <button onClick={function(){
+                console.log("return문 안쪽 익명함수")
+            }}>클릭3</button>
+        </div>
     );
 }
 
