bf194261880f8ee6d32814d3759170ba2873f8ce
Hwayeong <joid1004@naver.com>
2024-05-10 18:21:07 +0900

    onClick property와 함수 사용 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,19 @@ function App(props) {
         console.log("anonymous function. 변수 이름은 action2");
     };
 
+    const arrow = ()=>console.log("arrow");
+
     return (
         <div>
             {/*함수 이름이 action1*/}
-            <button onClick={action1}>액션1클릭</button><br/>
+            <button onClick={action1}>onClick property에 이름 있는 함수 넣기</button><br/>
             {/*익명함수 변수 이름이 action2*/}
-            <button onClick={action2}>액션2클릭</button><br/>
+            <button onClick={action2}>onClick property에 이름 없는 함수 넣기</button><br/>
             <button onClick={function(){
-                console.log("return문 안쪽 익명함수")
-            }}>클릭3</button>
+                console.log("onClick property에 return문 안쪽 익명함수 넣기")
+            }}>클릭3</button><br/>
+            <button onClick={arrow}>onClick property에 arrow function 변수에 담아서 넣기</button><br/>
+            <button onClick={()=>console.log("arrow함수")}>onClick property에 arrow function 바로 넣기</button>
         </div>
     );
 }
