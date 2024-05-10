b714041f8e381d558ab93213ab441053c16bd9e8
Hwayeong <joid1004@naver.com>
2024-05-10 16:10:31 +0900

    style props은 객체로 css property 명은 lowerCamelCase 로

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 function App(props) {
   return (
-    <div></div>
+    <div>
+      <h1 style={{
+          color:"red",
+          padding:"10px",
+          border:"2px dotted black",
+          textAlign:"center"
+      }}
+      >
+          react style</h1>
+    </div>
   );
 }
 
