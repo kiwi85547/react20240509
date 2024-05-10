7b8f86a701851e38481aaa40160ca7ab36f0c5e1
Hwayeong <joid1004@naver.com>
2024-05-10 16:17:41 +0900

    inline style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+function MyComp({color}){
+    return(
+        <div style={{color:color}}>
+            <h1>lorem</h1>
+            <p>Lorem ipsum dolor.</p>
+    </div>)
+}
 
 function App(props) {
   return (
-    <div></div>
+    <div>
+        <MyComp color={"green"}/>
+        <MyComp color={"aqua"}/>
+    </div>
   );
 }
 
