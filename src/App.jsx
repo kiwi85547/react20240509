b7cedfeca9f1504cb456396139477ba231fc2a13
Hwayeong <joid1004@naver.com>
2024-05-13 04:13:34 +0900

    얉은 복사, 깊은 복사

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,59 @@
 import React from 'react';
 
 function App(props) {
-    // 나머지 모두 문법으로 객체 복사
-    // 프로퍼티는 순서가 없음
-
+    // address 프로퍼티가 객체
     const a = {
-        name: "son",
-        age: 33,
-        city: "london",
+        name: "John",
+        age: 44,
+        address: {
+            street: "Lorem Ipsum",
+            city: "seoul",
+        }
     };
-    const b = a; // a와 b는 같은 객체 참조
-    b.age = 44;
-    console.log("b.age", b.age); // 44
-    // 같은 객체를 참조하고 있으므로 a의 값도 같이 변경됨
+
+    // 얕은 복사 (shallow copy)
+    // 가장 상위의 프로퍼티들만 복사. 안에 있는것은 복사되지 않음
+    const {...b} = a;
+    b.age = 55;
+    console.log("b.age", b.age); // 55
     console.log("a.age", a.age); // 44
 
-    // 구조분해할당이 없으면
-    const c = {
-        name: a.name,
-        age: a.age,
-        city: a.city,
+    b.address.city = "busan";
+    console.log("a.address.city", a.address.city); // busan
+    console.log("b.address.city", b.address.city); // busan
+
+    // 깊은 복사 (deep copy)
+    // 얉은 복사를 여러번 해서 해결
+    const {...c} = a; // 얉은 복사
+    const {...address1} = a.address; // 얉은 복사
+    c.address = address1; // 깊은 복사
+    c.address.city = "london";
+    console.log("a.address.city", a.address.city); // busan
+    console.log("c.address.city", c.address.city); // london
+
+    // 연습; 깊은 복사
+    const d = {
+        company: {
+            name: "apple",
+            location: "us",
+        },
+        name: "iphone",
+        price: 300,
     };
-    c.age = 55;
-    console.log("c.age", c.age); // 55
-    console.log("a.age", a.age); // 44
 
-    // 구조분해할당을 이용하면
-    // a의 나머지 모두 프로퍼티가 복사해서 들어감
-    // 객체의 각 프로퍼티를 복사
-    const {...d} = a;
-    d.age = 66;
-    console.log("d.age", d.age); // 66
-    // 각 프로퍼티의 값을 각각 복사해 담았으니
-    // a.age는 44로 남아있음
-    console.log("a.age", a.age); // 44
+    // 코드 작성 : d 객체를 e 객체로 깊은 복사
+    const {...e} = d;
+    const {...company1} = d.company;
+    e.company = company1;
 
-    // 연습:
-    const e = {
-        name: "lee",
-        country: "korea",
-        team: "paris",
-        salary: 700,
-        city: "seoul",
-    };
-    // 코드 작성!
-    const {...f} = e;
+    e.name = "galaxy";
+    e.company.name = "samsung";
 
-    e.city = "busan";
-    // {
-    //   name: "lee",
-    //     country: "korea",
-    //     team: "paris",
-    //     salary: 700,
-    //     city: "busan",
-    // }
-    console.log(e);
-    /*
-{
-  name: "lee",
-  country: "korea",
-  team: "paris",
-  salary: 700,
-  city: "seoul",
-}
- */
-    console.log(f);
+    console.log("d.name", d.name); // iphone
+    console.log("d.company.name", d.company.name); // apple
+    console.log("e.name", e.name); // galaxy
+    console.log("e.company.name", e.company.name); // samsung
+
 
     return (<></>
     );
