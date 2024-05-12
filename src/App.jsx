514b46b8351ed8183e4f963b0c1b3a94bad062fc
Hwayeong <joid1004@naver.com>
2024-05-13 04:26:49 +0900

    배열 복사를 분해할당의 나머지모두로 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,44 @@
 import React from 'react';
 
 function App(props) {
-    // address 프로퍼티가 객체
-    const a = {
-        name: "John",
-        age: 44,
-        address: {
-            street: "Lorem Ipsum",
-            city: "seoul",
-        }
-    };
+    // object({}, [])
+    const a = [6, 7, 8];
+    const [x, y, z] = a;
+    console.log("x", x); // 6
+    console.log("y", y); // 7
+    console.log("z", z); // 8
 
-    // 얕은 복사 (shallow copy)
-    // 가장 상위의 프로퍼티들만 복사. 안에 있는것은 복사되지 않음
-    const {...b} = a;
-    b.age = 55;
-    console.log("b.age", b.age); // 55
-    console.log("a.age", a.age); // 44
+    // 나머지 모두 (...변수명) rest
+    const [s, ...t] = a;
+    console.log("s", s); // 6
+    console.log("t", t); // [7, 8]
+    console.log("t[0]", t[0]); // 7
+    console.log("t[1]", t[1]); // 8
 
-    b.address.city = "busan";
-    console.log("a.address.city", a.address.city); // busan
-    console.log("b.address.city", b.address.city); // busan
+    // 나머지 모두로 배열 복사 가능
+    const c = a; // 참조값 복사
+    const [...b] = a; // 각 원소를 새 배열에 복사
+    console.log("b", b); // [6, 7, 8]
 
-    // 깊은 복사 (deep copy)
-    // 얉은 복사를 여러번 해서 해결
-    const {...c} = a; // 얉은 복사
-    const {...address1} = a.address; // 얉은 복사
-    c.address = address1; // 깊은 복사
-    c.address.city = "london";
-    console.log("a.address.city", a.address.city); // busan
-    console.log("c.address.city", c.address.city); // london
+    c[0] = 66;
 
-    // 연습; 깊은 복사
-    const d = {
-        company: {
-            name: "apple",
-            location: "us",
-        },
-        name: "iphone",
-        price: 300,
-    };
+    console.log("a[0]", a[0]); // 66
+    console.log("b[0]", b[0]); // 6
+    console.log("c[0]", c[0]); // 66
 
-    // 코드 작성 : d 객체를 e 객체로 깊은 복사
-    const {...e} = d;
-    const {...company1} = d.company;
-    e.company = company1;
+    // 연습 : 객체 복사
+    const d = ["pizza", "son", 77];
+    // 코드 작성
+    // 1. 참조값 복사 , 객체 e
+    const e = d;
+    // 2. 각 원소를 복사해서 새 배열 만들기, 객체 f
+    const [...f] = d;
 
-    e.name = "galaxy";
-    e.company.name = "samsung";
+    e[2] = 88;
 
-    console.log("d.name", d.name); // iphone
-    console.log("d.company.name", d.company.name); // apple
-    console.log("e.name", e.name); // galaxy
-    console.log("e.company.name", e.company.name); // samsung
-
+    console.log("d", d); // ["pizza", "son", 88]
+    console.log("e", e); // ["pizza", "son", 88]
+    console.log("f", f); // ["pizza", "son", 77]
 
     return (<></>
     );
