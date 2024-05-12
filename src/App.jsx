51306240c7a6432f09451a0615a40f59a079e2ca
Hwayeong <joid1004@naver.com>
2024-05-13 02:40:35 +0900

    배열의 값들을 분해할당, 없으면 기본값 할당

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,55 @@
 import React from 'react';
 
 function App(props) {
-    // destructuring assignment
-    const person = {
-        name: "John",
-        email: "john@example.com",
-        address: "127.0.0.1",
-    };
-    const {name, email} = person;
-    console.log("name", name);
-    console.log("email", email);
-    // console.log("address", address);
+    // object ({}, [])
+    // [] : 배열, 원소를 순서대로 저장한 객체
+    const a = [6, 7, 8];
+    const v1 = a[0];
+    const v2 = a[1];
+    const v3 = a[2];
 
-    const car = {
-        model: "genesis",
-        company: "hyundai",
-        price: 500,
-    };
-    const {model, company, color} = car;
-    console.log("model", model);
-    console.log("company", company);
-    console.log("color", color); // undefined
+    console.log("v1", v1); // 6
+    console.log("v2", v2); // 7
+    console.log("v3", v3); // 8
 
-    const house = {
-        city: "seoul",
-        country: "us",
-    };
-    // 없으면 기본값 할당
-    const {city, country = "korea!!", area = 987} = house;
-    console.log("city", city);
-    // country의 기본값, us가 출력됨. korea!! 아님.
-    console.log("country", country); // us
-    console.log("area", area); // 987
+    const [x, y, z] = a;
+    console.log("x", x);
+    console.log("y", y);
+    console.log("z", z);
 
+    // 연습 :
+    const b = ["pizza", 3.14, "lunch"];
+
+    // 적절한 코드 작성
+    const [x1, x2, x3] = b;
+
+    console.log("x1", x1); // pizza
+    console.log("x2", x2); // 3.14
+    console.log("x3", x3); // lunch
+
+    const c = ["son", "lee"];
+    const [a1] = c;
+
+    console.log("a1", a1);
+
+    const [b1, b2, b3] = c;
+    console.log("b3", b3);
+
+    const [c1, c2 = "park", c3 = "kim"] = c;
+    console.log("c1", c1); // son
+    // park 가 아님
+    console.log("c2", c2); // lee
+    console.log("c3", c3); // kim
+
+    // 연습
+    const d = ["name", "email", "phone"];
+    // 분해할당 코드 작성
+    const [d1, d2, d3, d4 = "city"] = d;
+
+    console.log("d1", d1); // name
+    console.log("d2", d2); // email
+    console.log("d3", d3); // phone
+    console.log("d4", d4); // city
 
     return (<></>
     );
