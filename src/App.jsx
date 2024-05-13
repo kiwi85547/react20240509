9dfd4fbd298aa2924f6ad355f5a8b797eaaddaf4
Hwayeong <joid1004@naver.com>
2024-05-13 11:02:43 +0900

    chakura ui component 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button>클릭</Button>
+        <Button>
+          알림 <Badge>new</Badge>
+        </Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
