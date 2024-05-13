505a18f32b0273d272568b06b896a7ea658e0175
Hwayeong <joid1004@naver.com>
2024-05-13 09:23:37 +0900

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+function MyComp({ name, age, children }) {
+  console.log(name); // son
+  console.log(age); // 33
+  console.log(children); // undefined
+  return (
+    <div>
+      <div>{name}</div>
+      <div>{age}</div>
+      <div>{children}</div>
+    </div>
+  );
+}
+
 function App(props) {
-  return <></>;
+  return (
+    <>
+      <MyComp name={"son"} age={33}>
+        {/* content는 children prop */}
+        <p>Lorem ipsum dolor.</p>
+      </MyComp>
+      <MyComp name={"lee"} age={22}>
+        <ul>
+          <li>Lorem.</li>
+          <li>Et!</li>
+        </ul>
+      </MyComp>
+    </>
+  );
 }
 
 export default App;
