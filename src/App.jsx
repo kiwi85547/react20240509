a07f4a3bf0d92016366da0a743c97fb1dd4ce289
Hwayeong <joid1004@naver.com>
2024-05-13 09:35:54 +0900

    component 다른 파일로 옮기기

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
 import React from "react";
-
-function MyComp({ name, age, children }) {
-  console.log(name); // son
-  console.log(age); // 33
-  console.log(children); // undefined
-  return (
-    <div>
-      <div>{name}</div>
-      <div>{age}</div>
-      <div>{children}</div>
-    </div>
-  );
-}
+import { MyComp } from "./MyComp.jsx";
 
 function App(props) {
   return (
     <>
-      <MyComp name={"son"} age={33}>
-        {/* content는 children prop */}
-        <p>Lorem ipsum dolor.</p>
-      </MyComp>
-      <MyComp name={"lee"} age={22}>
-        <ul>
-          <li>Lorem.</li>
-          <li>Et!</li>
-        </ul>
-      </MyComp>
+      <MyComp></MyComp>
     </>
   );
 }
