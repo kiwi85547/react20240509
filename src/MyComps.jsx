127a87055d118535a007ebaa000449febce9a666
Hwayeong <joid1004@naver.com>
2024-05-13 10:32:25 +0900

    react component를 만드는 방법들
    이름 있는 함수
    이름 없는 함수
    arrow function

--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,16 +1,10 @@
-export const myStr = "Korea";
-export const myAddress = "seoul";
-export const value3 = 99;
-export const value4 = {};
-export const value5 = [];
-export function action1() {
-  return null;
-}
-export const action2 = function () {
-  return null;
-};
-export const action3 = () => null;
+// react component 들
 
-function MyComp() {
-  return <div>Lorem.</div>;
+export function MyComp() {
+  return <div>mycomp</div>;
 }
+export const YourComp = function () {
+  return <div>yourcomp</div>;
+};
+
+export const ArrowComp = () => <div>arrow function component</div>;
