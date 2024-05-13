a44e71b83109e7354144984ba334598bd97c327a
Hwayeong <joid1004@naver.com>
2024-05-13 11:44:11 +0900

    상태가 변경될 때 컴포넌트(함수)가 다시 실행됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
 import React, { useState } from "react";
 
 function MyComp() {
-  let val = 0;
-
-  function increment() {
-    val++;
-    console.log("val", val);
+  const [val1, setVal1] = useState(0);
+  let val2 = 0;
+  function updateVal1() {
+    setVal1(val1 + 1);
   }
+
+  function updateVal2() {
+    val2++;
+  }
+  console.log("다시 호출됨");
+
   return (
     <div>
-      {val}
-      <button onClick={increment}>증가</button>
-    </div>
-  );
-}
-
-function MyComp2() {
-  // react component는 상태(state)가 바뀌면 다시 그려짐(re render)
-  // 각 component의 state는 react가 관리
-  // useState 함수는 배열을 리턴
-  // 첫번째 원소(0 index) : 상태값
-  // 두번째 원소(1 index) : 상태를 업데이트하는 함수
-  //    관습적으로 접두어는 set, state명을 UpperCamelCase로 바꿈.
-  // const [stateValue,updateStateValue] = useState();
-  const [val, setVal] = useState(0);
-
-  function increment() {
-    setVal(val + 1);
-  }
-  return (
-    <div>
-      {val}
-      <button onClick={increment}>증가</button>
+      <button onClick={updateVal1}>val1</button>
+      <button onClick={updateVal2}>val2</button>
     </div>
   );
 }
@@ -40,7 +24,6 @@ function App(props) {
   return (
     <div>
       <MyComp />
-      <MyComp2 />
     </div>
   );
 }
