07af59b29a142cb10e5a0a899c56475468fcfeb4
Hwayeong <joid1004@naver.com>
2024-05-13 10:27:47 +0900

    React Component의 impoort,export

--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,3 +1,16 @@
 export const myStr = "Korea";
 export const myAddress = "seoul";
 export const value3 = 99;
+export const value4 = {};
+export const value5 = [];
+export function action1() {
+  return null;
+}
+export const action2 = function () {
+  return null;
+};
+export const action3 = () => null;
+
+function MyComp() {
+  return <div>Lorem.</div>;
+}
