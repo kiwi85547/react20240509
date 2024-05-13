3e2f143365b8b678fd5510c2753694b5c7804932
Hwayeong <joid1004@naver.com>
2024-05-13 14:05:18 +0900

    상태예제1) 버튼 클릭하면 1씩 증가

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import React, { useState } from "react";
 
-function MyComp1() {
-  // 여러 상태가 있을 수 있음
-  const [val1, setVal1] = useState(0);
-  const [val2, setVal2] = useState(0);
-  console.log("다시 그려짐");
+function MyCounter() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <div>{val1}</div>
-      <div>
-        <button onClick={() => setVal1(val1 + 1)}>click1</button>
-      </div>
-      <div>{val2}</div>
-      <div>
-        <button onClick={() => setVal2(val2 + 1)}>click2</button>
-      </div>
+      {count}
+      <button onClick={() => setCount(count + 1)}>증가</button>
     </div>
   );
 }
 function App(props) {
   return (
     <div>
-      <MyComp1 />
+      <MyCounter />
     </div>
   );
 }
