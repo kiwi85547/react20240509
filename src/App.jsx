cf643a90e2337707e07e36460be1f368d3b8b084
Hwayeong <joid1004@naver.com>
2024-05-13 09:48:37 +0900

    import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+// import : 다른 파일에 있는 변수를 현재 파일에서 사용
+// 상대경로 . 현재경로
+import { value1, value2, value3, value4 } from "./MyValues.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>value1 : {value1}</div>
+      <div>value2 : {value2}</div>
+      <div>value2 : {value3}</div>
+      <div>value2 : {value4.team}</div>
+    </div>
+  );
 }
 
 export default App;
