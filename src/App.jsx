9ed06dbe4cf37beae171c45412a85aa9c39894f2
Hwayeong <joid1004@naver.com>
2024-05-14 09:13:10 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-  // event를 e로 줄여서 씀
-  function handleUpdateText(e) {
-    console.log(e.target.value);
-    setText(e.target.value);
-  }
-  return (
-    <div>
-      {/* EventHandler에 들어가는 함수는 handle...를 따름*/}
-      <input type="text" onChange={handleUpdateText} />
-      <p>{text}</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
