9b55dd1f2550390bc0779f709199fad8e48a5d34
Hwayeong <joid1004@naver.com>
2024-05-14 14:11:19 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
-
-const TextContext = createContext(null);
-
-function MyInput() {
-  const textHandler = useContext(TextContext);
-  return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => textHandler.updateText(e.target.value)}
-      />
-      <p>{textHandler.text}</p>
-    </div>
-  );
-}
-
-function MyText() {
-  const textHandler = useContext(TextContext);
-  return (
-    <div>
-      <p>{textHandler.text}</p>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-
-  function handleUpdateText(t) {
-    setText(t);
-  }
-  return (
-    <div>
-      {/*value가 객체가 됨. value는 string이 아니어도 됨. 모든지 될 수 있음*/}
-      <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
-        <MyInput />
-        <MyText />
-      </TextContext.Provider>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
