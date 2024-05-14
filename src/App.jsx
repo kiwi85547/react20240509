3a35a6f5584a6428fa63f1912f7dd65a06e5f773
Hwayeong <joid1004@naver.com>
2024-05-14 15:10:26 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("s");
-  }, []);
-  useEffect(() => {
-    console.log("count가 변경될 때 ", count);
-    // 함수안에서 count 변경하면 안됨. 무한루프
-    // setCount(count+1)
-    // dependency가 변경되지 않도록 주의
-  }, [count]);
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>클릭</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
