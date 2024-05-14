1dc189fe09fef408bd314e2ddc1253262b6eee66
Hwayeong <joid1004@naver.com>
2024-05-14 10:36:40 +0900

    "lift state up"으로 컴포넌터의 상태 유지 가능. 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 
-function MyBox({ color }) {
-  const [count, setCount] = useState(0);
+function MyBox({ color, count, onclick }) {
   return (
     <div>
       <div style={{ backgroundColor: color }}>
-        <button onClick={() => setCount(count + 1)}>up</button>
+        <button onClick={onclick}>up</button>
         <p>{count}</p>
       </div>
     </div>
   );
 }
-
+// todo : MyBox가 tree 에 떨어진 후 다시 붙어도 count 상태가 유지될 수 있도록 코드 수정
 function App(props) {
+  const [count, setCount] = useState(0);
   const [checked, setChecked] = useState(true);
+
+  // setCount를 바로 넘겨주지 않고 함수를 전달하는 방법을 더 많이 씀
+  function handleUpdateCount() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <input
@@ -21,7 +27,9 @@ function App(props) {
         value={checked}
         onChange={(e) => setChecked(e.target.checked)}
       />
-      {checked && <MyBox color={"#ccc"} />}
+      {checked && (
+        <MyBox color={"#ccc"} count={count} onclick={handleUpdateCount} />
+      )}
     </div>
   );
 }
