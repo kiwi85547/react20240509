d406d491086659956ad9db215899d8a4b8f41afb
Hwayeong <joid1004@naver.com>
2024-05-14 14:07:52 +0900

    context로 데이터(객체) 전달 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { createContext, useContext, useState } from "react";
 
-const TextContext = createContext(null);
+const MessageContext = createContext(null);
 
 function MyInput() {
-  const textHandler = useContext(TextContext);
+  const textHandler = useContext(MessageContext);
   return (
     <div>
       <input
         type="text"
+        value={textHandler.text}
         onChange={(e) => textHandler.updateText(e.target.value)}
       />
       <p>{textHandler.text}</p>
@@ -16,7 +17,7 @@ function MyInput() {
 }
 
 function MyText() {
-  const textHandler = useContext(TextContext);
+  const textHandler = useContext(MessageContext);
   return (
     <div>
       <p>{textHandler.text}</p>
@@ -32,11 +33,13 @@ function App(props) {
   }
   return (
     <div>
-      {/*value가 객체가 됨. value는 string이 아니어도 됨. 모든지 될 수 있음*/}
-      <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
+      {/*객체의 프로퍼티와 값으로 넣는 값이 같으면 생략 가능하다. 예) text:text*/}
+      <MessageContext.Provider
+        value={{ text: text, updateText: handleUpdateText }}
+      >
         <MyInput />
         <MyText />
-      </TextContext.Provider>
+      </MessageContext.Provider>
     </div>
   );
 }
