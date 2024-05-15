ec4b8ec637aa6edb2baab737f0be45071f27bf49
Hwayeong <joid1004@naver.com>
2024-05-15 19:59:25 +0900

    udemy 버튼 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,80 @@
-import React, { useState } from "react";
-import axios from "axios";
+import {useState} from "react";
+import "./styles.css";
 
-function App(props) {
-  const [customerId, setCustomerId] = useState("");
-  const [customerName, setCustomerName] = useState("");
-  function handleSearchClick() {
-    // 조회버튼 클릭시
-    // /api/customer?id=6 로 요청
-    axios
-      .get(`/api/customer?id=${customerId}`)
-      .then((response) => response.data)
-      .then((data) => setCustomerName(data));
-  }
+const content = [
+    [
+        "React is extremely popular",
+        "It makes building complex, interactive UIs a breeze",
+        "It's powerful & flexible",
+        "It has a very active and versatile ecosystem"
+    ],
+    [
+        "Components, JSX & Props",
+        "State",
+        "Hooks (e.g., useEffect())",
+        "Dynamic rendering"
+    ],
+    [
+        "Official web page (react.dev)",
+        "Next.js (Fullstack framework)",
+        "React Native (build native mobile apps with React)"
+    ],
+    [
+        "Vanilla JavaScript requires imperative programming",
+        "Imperative Programming: You define all the steps needed to achieve a result",
+        "React on the other hand embraces declarative programming",
+        "With React, you define the goal and React figures out how to get there"
+    ]
+];
 
-  return (
-    <div>
-      <div>
-        <input type="number" onChange={(e) => setCustomerId(e.target.value)} />
-        <button onClick={handleSearchClick}>조회</button>
-      </div>
-      <div>이름 : {customerName}</div>
-    </div>
-  );
+export default function App() {
+    const [activeContentIndex, setActiveContentIndex] = useState(0);
+
+    return (
+        <div>
+            <header>
+                <img src="react-logo-xs.png" alt="React logo"/>
+                <div>
+                    <h1>React.js</h1>
+                    <p>i.e., using the React library for rendering the UI</p>
+                </div>
+            </header>
+
+            <div id="tabs">
+                <menu>
+                    <button
+                        className={activeContentIndex === 0 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(0)}
+                    >
+                        Why React?
+                    </button>
+                    <button
+                        className={activeContentIndex === 1 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(1)}
+                    >
+                        Core Features
+                    </button>
+                    <button
+                        className={activeContentIndex === 2 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(2)}
+                    >
+                        Related Resources
+                    </button>
+                    <button
+                        className={activeContentIndex === 3 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(3)}
+                    >
+                        Related Resources
+                    </button>
+                </menu>
+                <div id="tab-content">
+                    <ul>
+                        {content[activeContentIndex].map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App;
