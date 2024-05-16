bcdd4cf0ef440ddb797abea9ca71157577bd7578
Hwayeong <joid1004@naver.com>
2024-05-16 15:22:19 +0900

    마이빈 응답 받기

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,10 @@ function App(props) {
     axios.get("/api/main42/sub5").then((res) => console.log(res.data));
   }
 
+  function handleClick6() {
+    axios.get("/api/main42/sub6").then((res) => console.log(res.data));
+  }
+
   return (
     <div>
       <button onClick={handleClick}>버튼</button>
@@ -35,6 +39,7 @@ function App(props) {
       <button onClick={handleClick4}>JSON 응답 받기</button>
       <br />
       <button onClick={handleClick5}>응답 받기</button>
+      <button onClick={handleClick6}>마이빈 응답 받기</button>
     </div>
   );
 }
