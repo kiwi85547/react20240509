da40253fcdb0dc9a9c2b31d934fbca48e843aa11
Hwayeong <joid1004@naver.com>
2024-05-16 12:41:02 +0900

    js 객체가 json 으로 직렬화됨 이 때 각 데이터타입이 사용됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  // json 데이터 타입
+  // 문자열, 수, 불리언, 객체, 배열, null
+
+  function handleClick1() {
+    const data = {
+      name: "son",
+      age: 33,
+      married: false,
+      team: { location: "london", name: "토트넘" },
+      item: [0, 1, 2, 3, 4, "가", "나", "다"],
+      address: null,
+    };
+    axios.post("/api/main41/sub5", data);
+  }
+
+  function handleClick2() {
+    axios.post("/api/main41/sub6", {
+      name: "son",
+      age: 33,
+    });
+  }
+
+  function handleClick3() {
+    axios.post("/api/main41/sub7", {
+      id: 1,
+      alphabet: { a: "a", b: "b" },
+      한글: ["가", "나", "다"],
+      point: null,
+      info: {
+        name: "jaehoo",
+        age: 7,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>json 데이터와 요청</button>
+      <button onClick={handleClick2}>click</button>
+      <br />
+      <button onClick={handleClick3}>json데이터 요청 연습</button>
+    </div>
+  );
 }
 
 export default App;
