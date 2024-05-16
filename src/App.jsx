ba9ff5ec0ba393019267a56bd27c4bf6e5aa9f00
Hwayeong <joid1004@naver.com>
2024-05-16 14:03:45 +0900

    children prop 이용하여 마우스를 올렸을 때 배경색 바꾸기

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,36 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
-function App(props) {
-  // json 데이터 타입
-  // 문자열, 수, 불리언, 객체, 배열, null
+// function MyButton(props) {
+//   return <button>{props.children}</button>;
+// }
+//
+function MyButton({ children }) {
+  const [bgColor, setBgColor] = useState();
 
-  function handleClick1() {
-    const data = {
-      name: "son",
-      age: 33,
-      married: false,
-      team: { location: "london", name: "토트넘" },
-      item: [0, 1, 2, 3, 4, "가", "나", "다"],
-      address: null,
-    };
-    axios.post("/api/main41/sub5", data);
+  function handleMouseEnter() {
+    setBgColor("green");
   }
 
-  function handleClick2() {
-    axios.post("/api/main41/sub6", {
-      name: "son",
-      age: 33,
-    });
-  }
-
-  function handleClick3() {
-    axios.post("/api/main41/sub7", {
-      id: 1,
-      alphabet: { a: "a", b: "b" },
-      한글: ["가", "나", "다"],
-      point: null,
-      info: {
-        name: "jaehoo",
-        age: 7,
-      },
-    });
+  function handleMouseLeave() {
+    setBgColor("");
   }
 
   return (
+    <button
+      style={{ background: bgColor }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {children}
+    </button>
+  );
+}
+
+function App(props) {
+  return (
     <div>
-      <button onClick={handleClick1}>json 데이터와 요청</button>
-      <button onClick={handleClick2}>click</button>
-      <br />
-      <button onClick={handleClick3}>json데이터 요청 연습</button>
+      <MyButton>클릭1</MyButton>
+      <MyButton>클릭2</MyButton>
     </div>
   );
 }
