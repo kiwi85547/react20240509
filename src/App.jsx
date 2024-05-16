22dce750331fc623f0de407f56934600f65b0b8c
Hwayeong <joid1004@naver.com>
2024-05-16 14:23:12 +0900

    응답받기(가위바위보)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [result, setResult] = useState();
+  function handleClick() {
+    axios.get("/api/main42/sub1").then((param) => console.log(param.data));
+  }
+
+  function handleClick2() {
+    axios.get("/api/main42/sub2").then((param) => setResult(param.data));
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>버튼</button>
+      {/*버튼이 클릭되면 /api/main42/sub2 get 요청 후 응답본문을 콘솔에 출력*/}
+      <button onClick={handleClick2}>응답 받기</button>
+      <br />
+      <p>{result}</p>
+    </div>
+  );
 }
 
 export default App;
