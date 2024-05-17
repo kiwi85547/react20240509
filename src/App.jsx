d18726afd17eaf54d06db92ca00d98afbcbc04d4
Hwayeong <joid1004@naver.com>
2024-05-17 15:01:47 +0900

    공통된 상위 경로로 묶기 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// spring/framework
+// spring/doc
+// spring/learn
 // 경로에 따라 다른 컴포넌트가 출력
 // 경로 앞 /은 생략가능
 const router = createBrowserRouter([
   { path: "about", element: <div>about 페이지</div> },
-  { path: "/search", element: <div>search 페이지</div> },
-  { path: "/home", element: <div>home 페이지</div> },
+  { path: "search", element: <div>search 페이지</div> },
+  { path: "home", element: <div>home 페이지</div> },
+  {
+    path: "spring",
+    children: [
+      { path: "framework", element: <div>spring framework</div> },
+      { path: "doc", element: <div>spring doc</div> },
+      { path: "learn", element: <div>spring learn</div> },
+    ],
+  },
 ]);
 function App(props) {
   return <RouterProvider router={router} />;
