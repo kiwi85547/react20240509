32f8254afd61993edce8dc3feb64fe227ce7a4b7
Hwayeong <joid1004@naver.com>
2024-05-17 15:02:40 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { BoardAdd } from "./BoardAdd.jsx";
-import { BoardEdit } from "./BoardEdit.jsx";
-import { BoardList } from "./BoardList.jsx";
-
-// router 객체
-// children 의 path에는 / 붙이면 안됨. 붙일거면 상위 /board까지 모두 붙여야 함
-const router = createBrowserRouter([
-  { path: "/add", element: <BoardAdd /> },
-  {
-    path: "/board",
-    children: [
-      { path: "edit", element: <BoardEdit /> },
-      { path: "list", element: <BoardList /> },
-    ],
-  },
-]);
 
 function App(props) {
-  return <RouterProvider router={router} />;
+  return <div></div>;
 }
 
 export default App;
