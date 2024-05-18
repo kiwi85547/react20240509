b839f451a456dd63ffd7459f87da715d32ec1dd9
Hwayeong <joid1004@naver.com>
2024-05-19 00:57:20 +0900

    input에 입력시 p에 출력, onChange,onMouseEnter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 function App(props) {
-  // input에 입력된 값이 p에 출력되도록 코드 작성
-  const [name, setName] = useState();
-  function handlerUpdate(e) {
-    setName(e.target.value);
-  }
+    // input에 입력된 값이 p에 출력되도록 코드 작성
+    const [name, setName] = useState();
+    const [text, setText] = useState();
 
-  return (
-    <div>
-      <input type="text" onChange={handlerUpdate} />
-      <p>{name}</p>
-    </div>
-  );
+    function handlerUpdate(e) {
+        setName(e.target.value);
+    }
+
+    function handleChange(e) {
+        setText(e.target.value)
+    }
+
+    return (
+        <div>
+            <input type="text" onChange={handlerUpdate} style={{color: "skyblue"}}/>
+            <p>{name}</p>
+            <div>
+                <input type="text" onChange={handleChange}/>
+                <p style={{color: "darkseagreen"}}>{text}</p>
+            </div>
+            <div>
+                <input type="text" onMouseEnter={e => setText(e.target.value)} style={{color: "darkred"}}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
