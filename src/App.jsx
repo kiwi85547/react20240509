d1ecef787e942faeabdf319bb36497ec845ed7c2
Hwayeong <joid1004@naver.com>
2024-05-20 18:33:43 +0900

    반응형 스타일

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import {Box, ChakraProvider} from "@chakra-ui/react";
 function App(props) {
     return (
         <ChakraProvider>
-            <Box m={1} bgColor={"orange.50"}>m=1</Box>
-            <Box m={10} bgColor={"orange.100"}>m=10</Box>
-            <Box m={20} bgColor={"orange.200"}>m=20</Box>
-            <Box color={"orange.300"}>color orange.300</Box>
-            <Box bgColor={"#f50"}>css 색상 값 사용</Box>
-            <Box bg={"orange.900"}>Hello everyone</Box>
+            <Box bg={"greenyellow"} w={"500px"}>w=500px</Box>
+            <br/>
+            // 작은 화면 (기본 값): 너비가 100%로 설정되어 화면의 전체 너비를 차지
+            // 중간 크기 이상의 화면 (md 이상): 너비가 500px로 고정됩니다.
+            <Box bg={"yellowgreen"} w={{base: "100%", md: "500px"}}>w=base: "100%", md: "500px"</Box>
         </ChakraProvider>
     );
 }
