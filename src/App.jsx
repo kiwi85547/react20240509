f0075cae09750976c69f9a0f385f822dfb036b04
Hwayeong <joid1004@naver.com>
2024-05-21 01:50:41 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
-import React from "react";
+import React from 'react';
 
 function App(props) {
-    // a 클릭 시
-    // form 서브밋 시
-
-    function handleClick(e) {
-        e.preventDefault();
-        console.log("앵커태그 말고 다른 일")
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log("form action 말고 다른 일")
-    }
-
-    // 기본 동작 막고,, 콘솔에 간단한 메세지 출력
     return (
-        <div>
-            <a href="https://www.google.com" onClick={handleClick}>구글</a>
-            <hr/>
-            <form action="https://www.google.com" onSubmit={handleSubmit}>
-                <input type="text"/>
-                <button>전송</button>
-            </form>
-        </div>
+        <div></div>
     );
 }
 
