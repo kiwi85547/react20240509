f9fa086d6463a953c6e5b1d5387940290f43af7e
Hwayeong <joid1004@naver.com>
2024-05-21 01:57:31 +0900

    file 전송
    method=post
    encType="multipart/form-data"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 function App(props) {
     return (
-        <div></div>
+        <div>
+            {/*파일 보낼 땐 꼭 method, encType 적기*/}
+            <form action="/path" method="POST" encType="multipart/form-data">
+                이름 <input type="text" name="path"/>
+                <br/>
+                <input type="file" name="file"/>
+                <br/>
+                <input type="submit"/>
+            </form>
+        </div>
     );
 }
 
