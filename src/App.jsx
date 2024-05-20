3eb20438a28c6b21a0c66ceae3c8d736b06fdee7
Hwayeong <joid1004@naver.com>
2024-05-21 00:49:47 +0900

    react hook(use...)은 컴포넌트 상단에 if/for문 없이 순서대로 작성할 것

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import React from 'react';
-import {Button, ChakraProvider, useToast} from "@chakra-ui/react";
-import {AddIcon, ArrowForwardIcon, ArrowRightIcon, SearchIcon, SmallAddIcon} from "@chakra-ui/icons";
+import React, {useEffect, useState} from 'react';
+import {ChakraProvider, useDisclosure, useToast} from "@chakra-ui/react";
 
 function App(props) {
+    // use... : Hook
+    // if / for 내에서 사용하지 말 것
+
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+    }, []);
     const toast = useToast();
+    const {isOpen, onClose, onOpen} = useDisclosure();
+    const [message, setMessage] = useState("")
+
     return (
         <ChakraProvider>
-            <ArrowRightIcon/><br/>
-            <SearchIcon boxSize={10}/><br/>
-            <AddIcon w={8} h={18} color={"red"}/><br/>
-            <Button ml={2} colorScheme={"teal"} rightIcon={<SmallAddIcon/>}
-                    onClick={() => toast({
-                        title: "토스트 제목",
-                        description: "We've created your account for you",
-                        //status: "info" 파란색 "success" 초록색
-                        status: "success",
-                        position: "bottom-left",
-                        isClosable: true,
-                        duration: 2000
-                    })}>계좌 생성
-            </Button>
-            <Button rightIcon={<ArrowForwardIcon/>} variant={"outline"}
-                    colorScheme={"teal"} ml={4}
-                    onClick={() => toast({
-                        description: "02-333-4444",
-                        status: "info",
-                        position: "top",
-                        duration: 3000,
-                        isClosable: true
-                    })}
-            >Call us</Button>
+            <div></div>
         </ChakraProvider>
     );
 }
