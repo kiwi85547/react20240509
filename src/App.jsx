1ee25b212d42ebaee70c5e1a801d8882e5bc4fb9
Hwayeong <joid1004@naver.com>
2024-05-20 21:36:26 +0900

    FormControl

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import {Box, Center, ChakraProvider, Flex, Spacer} from "@chakra-ui/react";
+import {Box, Center, ChakraProvider, FormControl, FormLabel, Input} from "@chakra-ui/react";
 
 function App(props) {
     return (
         <ChakraProvider>
-            <Flex justify={"center"}>
-                <Box bgColor={"coral"} w="100px">1</Box>
-                <Box bg={"red"} w="100px">1</Box>
-                <Spacer/>
-                <Box bg={"blue"} w="100px">1</Box>
-            </Flex>
-            <hr/>
-            <Flex justify={"space-evenly"}>
-                <Box bg={"coral"} w="100px">2</Box>
-                <Box bg={"red"} w="100px">2</Box>
-                <Box bg={"blue"} w="100px">2</Box>
-            </Flex>
+            <label htmlFor={"input1"}>이름</label>
+            <input id={"input1"}/>
             <hr/>
             <Center>
-                <Box w={{base: "100%", md: "1000px"}} bg={"lemonchiffon"}>Box3</Box>
+                <Box w={{base: "100%", md: "50%"}}>
+                    <FormControl>
+                        <FormLabel>이름</FormLabel>
+                        <Input/>
+                    </FormControl>
+                </Box>
             </Center>
-        </ChakraProvider>);
+        </ChakraProvider>
+    );
 }
 
 export default App;
