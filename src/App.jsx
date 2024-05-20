5ec8604b477565e687e776abea19ef2da8e08af1
Hwayeong <joid1004@naver.com>
2024-05-20 20:01:04 +0900

    Flex, Center

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import {Button, ChakraProvider} from "@chakra-ui/react";
+import {Box, Center, ChakraProvider, Flex, Spacer} from "@chakra-ui/react";
 
 function App(props) {
     return (
         <ChakraProvider>
-            {/*맞춤형으로 수정하고 싶으면 sx 쓰기. style props 사용하듯이 쓰면 됨*/}
-            <Button sx={{color: "orange"}} colorScheme={"blue"}>sx(system style object):스타일 덮어쓰기</Button>
-        </ChakraProvider>
-    );
+            <Flex justify={"center"}>
+                <Box bgColor={"coral"} w="100px">1</Box>
+                <Box bg={"red"} w="100px">1</Box>
+                <Spacer/>
+                <Box bg={"blue"} w="100px">1</Box>
+            </Flex>
+            <hr/>
+            <Flex justify={"space-evenly"}>
+                <Box bg={"coral"} w="100px">2</Box>
+                <Box bg={"red"} w="100px">2</Box>
+                <Box bg={"blue"} w="100px">2</Box>
+            </Flex>
+            <hr/>
+            <Center>
+                <Box w={{base: "100%", md: "1000px"}} bg={"lemonchiffon"}>Box3</Box>
+            </Center>
+        </ChakraProvider>);
 }
 
 export default App;
