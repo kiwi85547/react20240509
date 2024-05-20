c191b21fe87a72ae7e0959a4875d82cce44bb29d
Hwayeong <joid1004@naver.com>
2024-05-20 19:42:00 +0900

    sx : 스타일 덮어쓰기

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import {Box, Button, ChakraProvider} from "@chakra-ui/react";
+import {Button, ChakraProvider} from "@chakra-ui/react";
 
 function App(props) {
     return (
         <ChakraProvider>
-            <Box bg={"coral"} _hover={{bg: "yellow"}}>hover</Box>
-            
-            <Button
-                colorScheme="teal"
-                _hover={{
-                    background: "white",
-                    color: "teal.500",
-                }}
-            >
-                Hover me
-            </Button>
-
+            {/*맞춤형으로 수정하고 싶으면 sx 쓰기. style props 사용하듯이 쓰면 됨*/}
+            <Button sx={{color: "orange"}} colorScheme={"blue"}>sx(system style object):스타일 덮어쓰기</Button>
         </ChakraProvider>
     );
 }
