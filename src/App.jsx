0a5898ca763b3ceefcec5b19977844f35bf75c5b
Hwayeong <joid1004@naver.com>
2024-05-20 19:01:38 +0900

    Pseudo class : hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Box, ChakraProvider} from "@chakra-ui/react";
+import {Box, Button, ChakraProvider} from "@chakra-ui/react";
 
 function App(props) {
     return (
         <ChakraProvider>
-            <div style={{backgroundColor: "greenyellow", width: "500px"}}>w=500px</div>
-            <div style={{backgroundColor: "yellowgreen", width: "100%"}}>w=100%</div>
-            // 반응형 디자인. 너비가 500px이 넘어가면 blue, 안넘어가면 red
-            <Box bg={{base: "red", md: "blue"}} w={{base: "100%", md: "500px"}}>Responsive Content</Box>
+            <Box bg={"coral"} _hover={{bg: "yellow"}}>hover</Box>
+            
+            <Button
+                colorScheme="teal"
+                _hover={{
+                    background: "white",
+                    color: "teal.500",
+                }}
+            >
+                Hover me
+            </Button>
+
         </ChakraProvider>
     );
 }
