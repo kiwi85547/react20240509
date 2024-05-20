3217b66de51458d81b3c3fa7298bde6c20455ddd
Hwayeong <joid1004@naver.com>
2024-05-21 01:38:56 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import {Box, Button, ChakraProvider} from "@chakra-ui/react";
 
 function App(props) {
-    function handleBoxClick() {
-        console.log("박스 클릭됨")
-    }
-
-    function handleButtonClick(e) {
-        // event bubbling 멈추기
-        e.stopPropagation();
-        console.log("버튼 클릭됨")
-    }
-
     return (
-        <ChakraProvider>
-            <Box onClick={handleBoxClick} bgColor={"orange"} w={"100px"} h={"100px"}>
-                <Button onClick={handleButtonClick}>버튼</Button></Box>
-        </ChakraProvider>
+        <div></div>
     );
 }
 
