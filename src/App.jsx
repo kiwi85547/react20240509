2e980e478d122bfc1162f36bf49e53a1ad488432
Hwayeong <joid1004@naver.com>
2024-05-21 01:26:55 +0900

    이벤트 버블링 막기 stopPropagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {Box, Button, ChakraProvider} from "@chakra-ui/react";
 
 function App(props) {
+    function handleBoxClick() {
+        console.log("박스 클릭됨")
+    }
+
+    function handleButtonClick(e) {
+        // event bubbling 멈추기
+        e.stopPropagation();
+        console.log("버튼 클릭됨")
+    }
+
     return (
-        <div></div>
+        <ChakraProvider>
+            <Box onClick={handleBoxClick} bgColor={"orange"} w={"100px"} h={"100px"}>
+                <Button onClick={handleButtonClick}>버튼</Button></Box>
+        </ChakraProvider>
     );
 }
 
