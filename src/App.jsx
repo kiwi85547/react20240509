6d58beffc2b9c3ecdc6eec854fc27245426965f6
Hwayeong <joid1004@naver.com>
2024-05-21 01:37:51 +0900

    이벤트 버블링 막기 연습 완료 stopPropagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from "react";
+import {Badge, Box, Button, ChakraProvider} from "@chakra-ui/react";
 
 function App(props) {
+    function handleBadgeClick(e) {
+        e.stopPropagation();
+        console.log("BADGE 클릭")
+    }
+
+    function handleButtonClick(e) {
+        e.stopPropagation();
+        console.log("button 클릭")
+    }
+
+    function handleBoxClick() {
+        console.log("박스 클릭")
+    }
+
     return (
-        <div></div>
+        <ChakraProvider>
+            <Box onClick={handleBoxClick} w={"200px"} bgColor={"gray"} p={10}>
+                <Button onClick={handleButtonClick} colorScheme={"blue"}>
+                    클릭
+                    <Badge onClick={handleBadgeClick} ml={3}>
+                        new
+                    </Badge>
+                </Button>
+            </Box>
+        </ChakraProvider>
     );
 }
 
