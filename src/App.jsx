8b78b0e4a9543cfef2b55f51e74b8a2b04678b98
Hwayeong <joid1004@naver.com>
2024-05-20 22:12:39 +0900

    Form 컴포넌트들 checkbox,radio group,textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
-import {Box, Center, ChakraProvider, FormControl, FormLabel, Input} from "@chakra-ui/react";
+import React, {useState} from 'react';
+import {
+    Box,
+    Center,
+    ChakraProvider,
+    Checkbox,
+    FormControl,
+    FormLabel,
+    Input,
+    Radio,
+    RadioGroup,
+    Stack,
+    Switch,
+    Text,
+    Textarea
+} from "@chakra-ui/react";
+
 
 function App(props) {
+    const [resize, setResize] = useState('horizontal')
+    const [value, setValue] = useState("");
+
+    function handleInputChange(e) {
+        setValue(e.target.value)
+    }
+
     return (
         <ChakraProvider>
             <label htmlFor={"input1"}>이름</label>
@@ -15,6 +37,39 @@ function App(props) {
                     </FormControl>
                 </Box>
             </Center>
+            <hr/>
+            <Checkbox>react</Checkbox>{"  "}
+            <Checkbox>vue</Checkbox>
+            <hr/>
+            <RadioGroup>
+                <Radio value="1">Chat GPT</Radio>
+                <Radio value="2">Copilot</Radio>
+                <Radio value="3">Gemini</Radio>
+            </RadioGroup>
+            <hr/>
+            Switch<Switch/>
+            <Textarea placeholer="입력하세요"></Textarea>
+
+
+            <hr/>
+            <RadioGroup defaultValue={resize} onChange={setResize} mt={2} mb={2}>
+                <Stack direction='row' spacing={5}>
+                    <Radio value='horizontal'>Horizontal</Radio>
+                    <Radio value='vertical'>Vertical</Radio>
+                    <Radio value='none'>None</Radio>
+                </Stack>
+            </RadioGroup>
+
+            <Textarea
+                placeholder='Here is a sample placeholder'
+                size='sm'
+                resize={resize}
+            />
+
+
+            <Text mb={"8px"}>Value:{value}</Text>
+            <Textarea value={value} onChange={handleInputChange} placeholder={"Here is a placeholder"} size={"sm"}/>
+
         </ChakraProvider>
     );
 }
