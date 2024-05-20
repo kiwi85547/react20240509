45d24931496044ec756b14d986903909a9482d6e
Hwayeong <joid1004@naver.com>
2024-05-20 17:54:48 +0900

    Box, 컬러, 마진, 패딩, 너비, 높이

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
+import {Box, ChakraProvider} from "@chakra-ui/react";
 
 function App(props) {
-    const names = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    // 배열을 map 메소드 사용해서 컴포넌트들을 만들 때
-    // key prop을 사용해서 효율성을 높일 것
-    // key prop에 값은 주로 primary key가 사용됨
     return (
-        <div>
-            <ul>
-                {names.map((name, index) => (
-                    <li key={index}>{name}</li>))}
-            </ul>
-        </div>
+        <ChakraProvider>
+            <div style={{
+                padding: "50px",
+                margin: "10px",
+                border: "10px solid black",
+                background: "tomato"
+            }}> Hello world
+            </div>
+            <Box p={"20px"} m={"30px"} border={"5px dashed black"} bgColor={"greenyellow"}>Box</Box>
+            <Box bgColor={"orange.50"}>Hello everyone</Box>
+            <Box bgColor={"orange.100"}>Hello everyone</Box>
+            <Box bgColor={"orange.200"}>Hello everyone</Box>
+            <Box bgColor={"orange.300"}>Hello everyone</Box>
+            <Box bgColor={"orange.500"}>Hello everyone</Box>
+            <Box bgColor={"orange.900"}>Hello everyone</Box>
+            <Box m={"10px"} bgColor={"seagreen"}>margin </Box>
+            <Box mt={"10px"} bgColor={"seagreen"}>margin top</Box>
+            <Box mb={"10px"} bgColor={"seagreen"}>margin bottom</Box>
+            <Box ml={"100px"} bgColor={"seagreen"}>margin left</Box>
+            <Box mr={"100px"} bgColor={"seagreen"}>margin right</Box>
+            <Box mx={"100px"} bgColor={"seagreen"}>margin x</Box>
+            <Box my={"20px"} bgColor={"seagreen"}>margin y</Box>
+            <Box p={"20px"} bgColor={"blue.100"}>padding</Box>
+            <Box pt={"20px"} bgColor={"blue.200"}>padding top</Box>
+            <Box pb={"20px"} bgColor={"blue.300"}>padding bottom</Box>
+            <Box pl={"20px"} bgColor={"blue.400"}>padding left</Box>
+            <Box pr={"100px"} bgColor={"blue.500"}>padding right. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Ab accusantium alias aspernatur culpa expedita nisi nostrum ? 가나다라마바사아자차</Box>
+            <Box px={"100px"} bgColor={"blue.600"}>padding x</Box>
+            <Box py={"100px"} bgColor={"blue.700"}>padding y</Box>
+            <Box w={"500px"} bgColor={"yellow.200"}>width 500px</Box>
+            <Box h={"500px"} bgColor={"yellow.700"}>height 500px</Box>
+        </ChakraProvider>
     );
 }
 
